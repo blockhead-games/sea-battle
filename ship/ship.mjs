dcd4267d60318719d65f310b209a74e5397ec903
Jinus <iamjinus@gmail.com>
2018-02-03 22:03:02 +0300

    Подправил код корабля

--- ship/ship.mjs
+++ ship/ship.mjs
@@ -1,14 +1,25 @@
 'use strict';
 
 import {Block} from '../block'
+import {Rocket} from '../perk'
+
 
 export default class Ship {
-    /**
-     * @param {array} blocks
-     */
-    constructor(blocks) {
-
+    constructor(params) {
+        this.blocks = [];
+        this.blocks.push([]);
+        
+        for (let i = 0; i < 4; i++) {
+            var data = {ship: this, perk: new Rocket()};
+            this.blocks.push(new Block(data));
+        }
     }
 
+    static get rows() {
+        return 4;
+    }
 
+    static get cols() {
+        return 1;
+    }
 }
