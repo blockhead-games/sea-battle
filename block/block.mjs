566dbfa0e8932b0ba8da286bab1ce26ab2eb4a66
Jinus <iamjinus@gmail.com>
2018-02-03 22:02:16 +0300

    Подправил код блока

--- block/block.mjs
+++ block/block.mjs
@@ -1,15 +1,29 @@
 'use strict';
 
+import {Ship} from "../ship/index.mjs";
+import Perk from "../perk/perk.mjs";
+
 const health = 100;
 
 export default class Block {
 
     /**
-     * @param {Perk} perk
+     * @param {Object} params
      */
-    constructor(perk) {
+    constructor(params) {
+        const {ship, perk} = params || {};
+
+        if (!(ship instanceof Ship)) {
+            throw 'Корабль задан криво';
+        }
+
+        if (!(perk instanceof Perk)) {
+            throw 'Перк задан криво';
+        }
+
         this.health = health;
         this.perk = perk;
+        this.ship = ship;
     }
 
     set health(val) {
