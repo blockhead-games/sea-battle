4162c4a1f293809e0ee0cfea1e43f906efd29fa4
Jinus <iamjinus@gmail.com>
2018-02-03 22:04:06 +0300

    Добавил корабль на поле при инициализации для теста

--- battle/battle.mjs
+++ battle/battle.mjs
@@ -1,10 +1,14 @@
 'use strict';
 
 import {Field} from '../field';
+import {Ship} from '../ship';
 
 export default class Battle {
     constructor() {
         this.field = new Field();
+        let ship = new Ship();
+        
+        this.field.addShip('A3', Field.DIRECTION_BOTTOM, ship);
     }
 
     start() {
