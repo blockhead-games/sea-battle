950b47d39ec9c44cb406e97e73df9cec45af85e0
ViES <vies@list.ru>
2018-03-03 18:08:28 +0300

    Rename sources to kebab-case

--- telegram-bot/index.mjs
+++ telegram-bot/index.mjs
@@ -1,3 +1,3 @@
 
 export {default as TelegramBot} from './telegram-bot';
-export {default as TelegramBotController} from './telegram-bot-controller';
+export {default as TelegramBotController} from './telegram-bot.controller';
